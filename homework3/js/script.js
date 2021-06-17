497be6ecf2fc407177d19f8b356027af4343873a
Tanya-kl <frolova.tetyana@gmail.com>
2021-06-17 01:40:43 +0000

    Create script.js

---
+++ homework3/js/script.js
@@ -0,0 +1,16 @@
+let admin;
+let name;
+name= "Джон";
+admin="Джон"
+//admin= name;
+alert(admin);
+
+let thirdPlanetOfTheSolarSystem='Земля';
+let currentUserName='Юрий';
+alert(thirdPlanetOfTheSolarSystem);
+alert(currentUserName);
+
+let string10 = '10';
+let number10= 10;
+let result= string10 + number10;
+console.log(string10,typeof string10,number10, typeof number10, result, typeof result);
