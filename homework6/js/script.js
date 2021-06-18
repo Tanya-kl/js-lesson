ca26be9eda4960f408ad74df3d623e66d5a14001
Tanya-kl <frolova.tetyana@gmail.com>
2021-06-18 12:43:27 +0000

    Create script.js

---
+++ homework6/js/script.js
@@ -0,0 +1,26 @@
+//Boolean(Number(’10’)) + 1);//2
+//‘sub ’ + Number(false);//'sub0'
+//16  *  ‘      91    ‘;//1496
+//true-70;//-69
+//Number(1 + String(1)) + 1;//12
+
+let quantityOfHours = Number(prompt('Введите количество часов'));
+let quantitySecInHour = quantityOfHours * 3600;
+let result = quantitySecInHour;
+alert(result);
+console.log(typeof quantitySecInHour, quantityOfHours);
+
+var num = 1;
+num += 12;
+alert(num);
+num -= 14;
+alert(num);
+num *= 5;
+alert(num);
+num /= 7;
+alert(num);
+num ++;
+alert (num);
+num --;
+alert(num);
+
