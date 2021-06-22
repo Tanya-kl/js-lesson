bfc054a946b8d446d207bb61a268de7c41da7dc4
Tanya-kl <frolova.tetyana@gmail.com>
2021-06-22 12:13:34 +0000

    Create script.js

---
+++ homework7_/js/script.js
@@ -0,0 +1,43 @@
+//let a = prompt('Введите число');
+//a == 0 ? alert ('Верно'): alert ('Не верно');
+//a > 0 ? alert ('Верно'): alert ('Не верно');
+//a < 0 ? alert ('Верно'): alert ('Не верно');
+//a >= 0 ? alert ('Верно'): alert ('Не верно');
+//a <= 0 ? alert ('Верно'): alert ('Не верно');
+//a != 0 ? alert ('Верно'): alert ('Не верно');
+//a ==='test' ? alert ('Верно'): alert ('Не верно');
+//a == 1 ? alert ('Верно'): alert ('Не верно');
+//(a > 0 && a < 5) ? alert ('Верно'): alert ('Не верно');
+
+//let a = +prompt('Введите число');
+//a ===(0||2) ? alert(a + 7) : alert (a / 10);
+
+
+//let a = +prompt('Введите первое число ');
+//let b = +prompt('Введите второе число ');
+//let c = (a <= 1, b >= 3)? alert (a + b) : alert(a - b);
+
+
+//let a = prompt('Введите первое число');
+//let b = prompt('Введите второе число');
+//(a > 2 && a < 11) ? alert('Верно') : alert ('Не верно');
+//(b < 14 && b >= 6) ? alert('Верно') : alert ('Не верно');
+
+
+//let num = prompt('Введите число');
+//switch (num) {
+  //  case '1':
+  //      alert('весна');
+  //      break;
+  //  case '2':
+    //  alert('лето');
+ //     break;
+  //  case '3':
+   //   alert('осень');
+ //     break;
+ //   case '4':
+  //    alert('зима');
+  //    break;
+  // default:
+    // alert('нет такого варианта');
+//}
