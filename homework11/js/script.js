5191c1ac9175cde3b9983a162e0f39131e609dcd
Tanya-kl <frolova.tetyana@gmail.com>
2021-06-28 16:09:07 +0000

    Create script.js

---
+++ homework11/js/script.js
@@ -0,0 +1,18 @@
+let num = +prompt('Введите Ваше число');
+
+for (let i = 0; i < num; i++) {
+    if (num % i === 0) {
+        alert(true);
+    }
+    else if (num % num === 0 ) {
+        alert(true);
+    }
+    else if (num = 1) {
+        alert (false);
+    }
+    else {
+        alert (false);
+    }
+}
+
+
