5c11ce0dd9b0107a95aa0f6ef285b9acebbc59e1
Tanya-kl <frolova.tetyana@gmail.com>
2021-07-12 13:41:31 +0000

    Create script.js

---
+++ homework20/js/script.js
@@ -0,0 +1,79 @@
+
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+console.log( arr1.concat(arr2) );
+//console.log( arr1.reverse();
+
+// (arr1.push(4, 5, 6));
+// console.log(arr1);
+
+// ( arr1.unshift(4, 5, 6));
+// console.log(arr1);
+
+
+//let arr4 = [1, 2, 3, 4, 5];
+//  arr4.splice(3, 0, 'a','b','c');
+// console.log(arr4);
+
+
+ // arr4 = [1, 2, 3, 4, 5];
+ // arr4.splice(1, 0, 'a','b');
+ // arr4.splice(6, 0, 'c');
+ // arr4.splice(8, 0, 'e');
+ // console.log(arr4);
+
+//let arr3 = ['js', 'css', 'jq'];
+
+//console.log( arr3[0] );
+//console.log( arr3[arr3.length-1] );
+
+
+//let arr7 = [1, 2, 3, 4, 5];
+//console.log( arr7.slice(0,3));
+//console.log(arr7.slice(3));
+//console.log(arr7.splice(1,3));
+
+
+// let arr8 = [3, 4, 1, 2, 7];
+// console.log(arr8.sort());
+
+
+//let arr5 =  [5, 6, 7, 8, 9];
+
+// let sum = 0;
+// for(let i = 0; i < arr5.length; i++) {
+// sum += arr5[i];
+// }
+// console.log(sum);
+
+// let newArr = [];
+// arr5.forEach(function (elem){
+//     newArr.push(elem * elem);
+// });
+// console.log(newArr);
+
+
+
+//let arr6 = [1,-3, 5, 6,-7, 8, 9,-11];
+// function func(elem){return elem < 0};
+// let newArr = arr6.filter(func);
+// alert(newArr);
+
+// function func(elem){return elem % 2 === 0};
+// let newArr = arr6.filter(func);
+// alert(newArr);
+
+// let arr = ['aaa', 'aaaqqq', 'zzzqq', 'zz', 'qsaa', 'q', 'az'];
+// function func(elem){return elem.length > 5};
+// let newArr = arr.filter(func);
+// alert(newArr);
+
+// let arr = [1, 2, [3, 4], 5, [6, 7]];
+// function func(elem){return Array.isArray(elem)};
+// let newArr = arr.filter(func);
+// alert(newArr);
+//
+// let arr = [5, -3, 6, -5, 0, -7, 8, 9];
+// function func(elem){return elem < 0};
+// let newArr = arr.filter(func);
+// alert(newArr.length);
