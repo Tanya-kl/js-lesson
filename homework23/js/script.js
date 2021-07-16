2388125540ee326939ca9b34781a532cc6a2c531
Tanya-kl <frolova.tetyana@gmail.com>
2021-07-16 18:01:49 +0000

    Create script.js

---
+++ homework23/js/script.js
@@ -0,0 +1,33 @@
+// 1
+
+// function factorial(n){
+//     if (n===1  ||n===0 ){
+//         return 1;
+//     } else {
+//         return n * factorial( n -1);
+//     }
+// }
+// factorial(3)
+// console.log(factorial(3));
+
+//2
+
+// function pow(num, deg){
+//     if (deg === 1){
+//         return num;
+//     }
+//     return num * pow(num, deg-1);
+// }
+// pow(10, 2);
+// console.log(pow(10, 2));
+
+//3
+
+function sum(a, b){
+    for (let i = 0; i < a;i++)
+       if ( b === 0) break;
+
+       return a
+
+}
+sum();
