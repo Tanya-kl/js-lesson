ec666e4ba7e8c51039a34a09000ff43f2bf6be1a
Tanya-kl <frolova.tetyana@gmail.com>
2021-07-19 17:25:21 +0000

    Update script.js

--- homework14/js/script.js
+++ homework14/js/script.js
@@ -1,20 +1,32 @@
 
-let string = prompt('Введите Вашу строку');
-let number = prompt('Введите желаемую длинну строки');
-let symb = Symbol("*");
-let sub = string.substr(0, number);
-
-function padString(string, number, symb, Boolean='right') {
-    if (string === typeof('undefined') && number === typeof('undefined')){
-        alert('Вы не ввели строку');
-    } else if (isNaN(number)) {
-        alert('Вы ввели не число');
-    } else if (number > string){
-        alert(string+ $`symb`);
-    } else if (string > number) {
-        alert(sub);
+function padString(string, strLength, padStr, padSide = true) {
+    let result = "";
+    if (typeof string !== 'string') {
+        return "Error with argument string"
+    }
+    if (typeof strLength !== 'number') {
+        return "Error with argument strLength"
+    }
+    if (typeof padStr !== 'string') {
+        return "Error with argument padStr"
+    }
+    if (typeof padSide !== 'boolean') {
+        return "Error with argument padSide"
+    }
+    if (padSide === true && string.length < strLength) {
+        result += string;
+        for (let i = 0; i < strLength - string.length; i++) {
+            result += padStr;
+        }
+        return result;
+    } else if (padSide === false && string.length < strLength) {
+        for (let i = 0; i < strLength - string.length; i++) {
+            result += padStr;
+        }
+        result += string;
+        return result;
+    } else {
+        return string.substr(0, strLength);
     }
 }
-
-padString(string, number, Boolean='right',symb);
-console.log(string, number, Boolean='right',symb);
+console.log(padString('hello', 8, '*', 'right'));
